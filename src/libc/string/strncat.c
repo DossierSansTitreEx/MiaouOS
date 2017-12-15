fafc21ac17f82b001795d99302580fe4af20a6c9
Nax <max@bacoux.com>
2017-12-15 13:41:49 +0100

    Add strncat

--- src/libc/string/strncat.c
+++ src/libc/string/strncat.c
@@ -1,6 +1,6 @@
 #include <string.h>
 
-char* strcat(char* __restrict s1, const char* __restrict s2)
+char* strncat(char* __restrict s1, const char* __restrict s2, size_t n)
 {
     size_t i;
     size_t j;
@@ -11,7 +11,7 @@ char* strcat(char* __restrict s1, const char* __restrict s2)
     while (s1[i])
         i++;
 
-    while (s2[j])
+    while (s2[j] && j < n)
     {
         s1[i + j] = s2[j];
         j++;
